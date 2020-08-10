51bcbc6aa63f5649a0d85cd69d6b5414488b622b
felipekafuri <felipekafuri@discente.ufg.br>
2020-08-10 15:44:21 -0300

    user list of booked appointmetns

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from 'tsyringe';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequestDTO {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+  ) { }
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequestDTO): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+        day,
+      }
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
