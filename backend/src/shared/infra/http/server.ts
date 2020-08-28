4ba45c0fcd04c404856b8200254b20b4325f1371
felipekafuri <felipekafuri@discente.ufg.br>
2020-08-27 21:45:46 -0300

    server

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -13,14 +13,13 @@ import '@shared/container';
 import rateLimiter from './middlewares/retelimiter';
 
 const app = express();
-app.use(rateLimiter);
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
